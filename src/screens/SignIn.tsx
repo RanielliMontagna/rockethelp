f700ff2191f93ff22f2b8a3ceddbf8079da2b393
Ranielli <raniellimontagna@hotmail.com>
2022-07-17 21:23:18 -0300

    feat: implementa inicio do app `rocket-help`

---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { VStack, Heading, Icon, useTheme } from "native-base";
+import { Envelope, Key } from "phosphor-react-native";
+
+import Logo from "../assets/logo_primary.svg";
+import { Button } from "../components/Button";
+import { Input } from "../components/Input";
+
+export function SignIn() {
+  const { colors } = useTheme();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+      <Logo />
+
+      <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
+        Acesso sua conta
+      </Heading>
+
+      <Input
+        placeholder="E-mail"
+        mb={4}
+        InputLeftElement={
+          <Icon as={<Envelope color={colors.gray[300]} />} ml={4} />
+        }
+        onChangeText={setEmail}
+      />
+      <Input
+        placeholder="Senha"
+        mb={8}
+        secureTextEntry
+        InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
+        onChangeText={setPassword}
+      />
+      <Button title="Entrar" w="full" />
+    </VStack>
+  );
+}
